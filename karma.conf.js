33406ce422fdaf2e8430dd4f74d8b1b254936034
Noah-Dev <ark_code@protonmail.com>
2017-11-02 14:43:03 -0500

    Phased out bower in favor of NPM completely

--- karma.conf.js
+++ karma.conf.js
@@ -15,12 +15,12 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      './bower_components/angular/angular.min.js',
-      './bower_components/angular-animate/angular-animate.min.js',
-      './bower_components/angular-aria/angular-aria.min.js',
-      './bower_components/angular-material/angular-material.min.js',
-      './bower_components/angular-material/angular-material.min.css',
-      './bower_components/angular-mocks/angular-mocks.js',
+      './node_modules/angular/angular.min.js',
+      './node_modules/angular-animate/angular-animate.min.js',
+      './node_modules/angular-aria/angular-aria.min.js',
+      './node_modules/angular-material/angular-material.min.js',
+      './node_modules/angular-material/angular-material.min.css',
+      './node_modules/angular-mocks/angular-mocks.js',
       
       './js/*.js'
     ],
